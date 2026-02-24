9ac11bcf54ec4cf0eb4d0d24f1373a6c41163243
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-02-24 09:03:47 +0100

    useReducer

--- my-react-app/src/6-useReducer/setup/index.jsx
+++ my-react-app/src/6-useReducer/setup/index.jsx
@@ -1,32 +1,38 @@
 import React, { useReducer, useState } from "react";
 import Modal from "./Modal";
-import { data } from "../../data";
+import { reducer } from "./reducer";
+// import { data } from "../../data";
 // reducer function
 
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
 const Index = () => {
-  
-  const reducer = (state, action)=> {
-    
-  }
   const [name, setName] = useState("");
-  const [state, dispatch] = useReducer(reducer)
+  const [state, dispatch] = useReducer(reducer, defaultState);
   // const [people, setPeople] = useState(data);
   // const [showModal, setShowModal] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true);
-      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
       setName("");
     } else {
-      setShowModal(true);
+      dispatch({ type: "NO_VALUE" });
     }
   };
-
+ const closeModal = () => {
+    dispatch({type:"CLOSE_MODAL"})
+  }
+ 
   return (
     <>
-      {showModal && <Modal />}
+      {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent} />}
       <form className="form" onSubmit={handleSubmit}>
         <div>
           <input
@@ -37,10 +43,11 @@ const Index = () => {
         </div>
         <button type="submit">add</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <button onClick={()=> dispatch({type: "REMOVE_ITEM", payload:person.id})}>remove</button>
           </div>
         );
       })}
